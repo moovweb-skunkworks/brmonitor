8e93247e999a04a04a058d7870842054f86623f1
Amit <amit.joshi@moovweb.com>
2014-05-22 22:12:23 -0700

    All the things!

--- assets/application.js
+++ assets/application.js
@@ -1,6 +1,68 @@
+HTMLElement.prototype.createShadowRoot = HTMLElement.prototype.webkitCreateShadowRoot ||
+                                         HTMLElement.prototype.mozCreateShadowRoot ||
+                                         HTMLElement.prototype.createShadowRoot;
+
+var Bathroom = function() {
+	this.stalls = [];
+}
+
+Bathroom.prototype.getStalls = function() {
+	var bathroom = this;
+	var stallsRequest = new XMLHttpRequest();
+	stallsRequest.open("GET", "/stalls", true);
+	stallsRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+	stallsRequest.addEventListener("load", function() {
+		var bathroomResponse = JSON.parse(this.responseText);
+		if(bathroomResponse.stalls !== bathroom.stalls) {
+			bathroom.stalls = bathroomResponse.stalls;
+			bathroom.render();
+		}
+	});
+
+	stallsRequest.send();
+}
+
+Bathroom.prototype.makeStall = function(stall, i, stalls) {
+	var stallTemplate = document.querySelector("template#stall");
+	stallTemplate.createShadowRoot();
+	var stallHtml = stallTemplate.content.querySelector(".stall").cloneNode(true);
+
+	if(stall.id == 1) {
+		stallHtml.classList.add("big");
+	}
+	if(stall.status == true) {
+		stallHtml.classList.add("open");
+	}
+	stallHtml.querySelector(".stall-id").innerHTML = stall.id.toString();
+
+	return stallHtml;
+}
+
+Bathroom.prototype.render = function() {
+	var display = document.querySelector(".status-display");
+	var brTemplate = document.querySelector("template#bathroom");
+	brTemplate.createShadowRoot();
+	var brHtml = brTemplate.content.querySelector(".bathroom").cloneNode(true);
+
+	var stallsHtml = this.stalls.map(this.makeStall);
+
+	stallsHtml.forEach(function(el) {
+		brHtml.appendChild(el);
+	});
+
+	display.innerHTML = "";
+	display.appendChild(brHtml);
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
-  alert("did this work?");
-  document.trigger("ready");
+	window.bathroom = new Bathroom();
+	window.setInterval(function() {
+		window.bathroom.getStalls();
+	},
+	2000);
+	console.log(bathroom.getStalls());
 });
 
-// docu
+
--- src/brmonitor/bathroom.go
+++ src/brmonitor/bathroom.go
@@ -1,116 +1,118 @@
 package brmonitor
 
-import(
-  "encoding/json"
-  "net/http"
-  "fmt"
-  "strings"
-  "strconv"
-  "io/ioutil"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Bathroom struct {
-  Stalls []Stall `json:"stalls"`
+	Stalls []Stall `json:"stalls"`
 }
 
 type FromJson struct {
-  Status bool `json:"status"`
+	Status bool `json:"status"`
 }
 
 // {
 //   "status": "false"
 // }
 
-func (bathroom Bathroom) GetStalls() (string) {
-  data, err := json.Marshal(bathroom)
+func (bathroom Bathroom) GetStalls() string {
+	data, err := json.Marshal(bathroom)
 
-  if err != nil {
-    panic(err.Error())
-  }
+	if err != nil {
+		panic(err.Error())
+	}
 
-  return string(data)
+	return string(data)
 }
 
 func (bathroom *Bathroom) AddStall() {
-  // write to db the stall. 
-  newStall := &Stall{}
-  newStall.Status = true
+	// write to db the stall.
+	newStall := &Stall{}
+	newStall.Status = true
 
-  newStall.Id = len(bathroom.Stalls) + 1
+	newStall.Id = len(bathroom.Stalls) + 1
 
-  fmt.Println(newStall.ToJSON())
+	fmt.Println(newStall.ToJSON())
 
-  bathroom.Stalls = append(bathroom.Stalls, *newStall)
+	bathroom.Stalls = append(bathroom.Stalls, *newStall)
 }
 
 func (bathroom Bathroom) HandleStalls(writer http.ResponseWriter, request *http.Request) {
-  if request.Method == "GET" {
-    fmt.Fprintf(writer, "%s", bathroom.GetStalls())
-  } else if request.Method == "PUT" {
-    currPath := request.URL.Path
+	if request.Method == "GET" {
+		writer.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(writer, "%s", bathroom.GetStalls())
+	} else if request.Method == "PUT" {
+		currPath := request.URL.Path
 
-    pathEls := strings.Split(currPath, "/")
-    if len(pathEls) == 3 {
-      pathId, err := strconv.Atoi(pathEls[len(pathEls) - 1])
-      if err != nil {
-        thatsShitty(err.Error(), writer, request)
-      } else {
-        // update dat thingieeeeeeeeeeeeeeeeeee
-        // fmt.Fprintf(writer, "%d", pathId)
-        stall, err := bathroom.GetStall(pathId)
-        if err != nil {
-          thatsShitty(err.Error(), writer, request)
-        } else {
-          body, err := ioutil.ReadAll(request.Body)
-          if err != nil {
-            thatsShitty(err.Error(), writer, request)
-          } else {
-            // ody = string(body)
-            put := &FromJson{}
-            err := json.Unmarshal(body, put)
+		pathEls := strings.Split(currPath, "/")
+		if len(pathEls) == 3 {
+			pathId, err := strconv.Atoi(pathEls[len(pathEls)-1])
+			if err != nil {
+				thatsShitty(err.Error(), writer, request)
+			} else {
+				// update dat thingieeeeeeeeeeeeeeeeeee
+				// fmt.Fprintf(writer, "%d", pathId)
+				stall, err := bathroom.GetStall(pathId)
+				if err != nil {
+					thatsShitty(err.Error(), writer, request)
+				} else {
+					body, err := ioutil.ReadAll(request.Body)
+					if err != nil {
+						thatsShitty(err.Error(), writer, request)
+					} else {
+						// ody = string(body)
+						put := &FromJson{}
+						err := json.Unmarshal(body, put)
 
-            if err != nil {
-              thatsShitty(err.Error(), writer, request)      
+						if err != nil {
+							thatsShitty(err.Error(), writer, request)
 
-            } else {
-              stall.SetStatus(put.Status)
-              fmt.Fprintf(writer, "%s", stall.ToJSON())
-            }
-          }
-        }
-      }
-    } else {
-      thatsShitty("URL Format Incorrect.", writer, request)      
-    }
-    // fmt.Fprintf(writer, "%s", currPath)
-  }
+						} else {
+							stall.SetStatus(put.Status)
+							writer.Header().Set("Content-Type", "application/json")
+							fmt.Fprintf(writer, "%s", stall.ToJSON())
+						}
+					}
+				}
+			}
+		} else {
+			thatsShitty("URL Format Incorrect.", writer, request)
+		}
+		// fmt.Fprintf(writer, "%s", currPath)
+	}
 }
 
 func (bathroom Bathroom) GetStall(stallId int) (*Stall, error) {
-  if len(bathroom.Stalls) > stallId-1 && stallId > 0 {
-    return &bathroom.Stalls[stallId-1], nil
-  } else {
-    return nil, fmt.Errorf("No stall with that ID %d", stallId)
-  }
+	if len(bathroom.Stalls) > stallId-1 && stallId > 0 {
+		return &bathroom.Stalls[stallId-1], nil
+	} else {
+		return nil, fmt.Errorf("No stall with that ID %d", stallId)
+	}
 }
 
 func thatsShitty(msg string, writer http.ResponseWriter, request *http.Request) {
-  http.Error(writer, "Page Not Found " + msg, 404)
+	http.Error(writer, "Page Not Found "+msg, 404)
 }
 
 // POST /stalls/2/update
 // bathroom.GetStall(2)
 // update it
-// reply with new stall. 
+// reply with new stall.
 // {
-  // stall: {
-     // status: false 
-  // }
+// stall: {
+// status: false
+// }
 // }
 
 // stall[status]=false
 
-// 1: Stall w/ id = 1 
-// 2: 
+// 1: Stall w/ id = 1
+// 2:
 
 // Bathroom.FindById(4)
